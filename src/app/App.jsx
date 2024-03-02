9db7e69390564c51c0a16c9da58109ea2a29cc59
souhhh <bulkin-2023@internet.ru>
2024-03-02 20:03:14 +0300

    gies

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import Menu from '../pages/Menu';
 import './styles/style.scss';
 import Home from '../pages/Home';
+import { Routes, Route } from 'react-router';
 function App() {
-  return <Home />;
+  return (
+    <>
+    <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            
+    </Routes>
+    </>
+  );
+
 }
 
 export default App;
