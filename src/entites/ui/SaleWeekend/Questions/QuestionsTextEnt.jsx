daa944e9daa2ad40a59b092a3ab44163bfe615a3
G0d-0f-war <tradeveyron@gmail.com>
2024-03-05 15:23:46 +0500

    1

---
+++ src/entites/ui/SaleWeekend/Questions/QuestionsTextEnt.jsx
@@ -0,0 +1,11 @@
+import styles from './QuenstionsTextEnt.module.scss';
+import Lines from '../../../../app/assets/img/sale/Illustration 01.svg';
+export default function QuenstionsTextEnt() {
+  return (
+    <div className={styles.QuestionsBlock}>
+      <img src={Lines} alt="" />
+      <div className={styles.Asked}>Frequently Asked</div>
+      <div className={styles.Questions}>Questions</div>
+    </div>
+  );
+}
