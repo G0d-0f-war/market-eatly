bd7ed005cb2582c76e24475916c61b2bdb948054
G0d-0f-war <tradeveyron@gmail.com>
2024-03-06 18:13:50 +0500

    123

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,13 @@
-import Sale from "../widgets/ui/Header/Sale/Sale";
-import Header from "../widgets/ui/Header/Header";
-export default function Menu () { 
-    return ( 
-        <>
-        <Header />
-        <Sale />
-        </>
-    )
-};
+import Sale from '../widgets/ui/Header/Sale/Sale';
+import Header from '../widgets/ui/Header/Header';
+import Questions from '../widgets/ui/Header/Questions/Questions';
+
+export default function Menu() {
+  return (
+    <>
+      <Header />
+      <Sale />
+      <Questions />
+    </>
+  );
+}
