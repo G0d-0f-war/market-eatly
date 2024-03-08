fd2d5b12c3261afa7f79c1d1abcc65c098408b33
G0d-0f-war <tradeveyron@gmail.com>
2024-03-09 01:18:27 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,8 +15,8 @@ function Home(props) {
       <Hero />
       <Statistics />
       <Quality />
-      <Restaurants variant={1} />
-      <Dishes variant={1} />
+      <Restaurants />
+      <Dishes />
       <Control />
       <Footer />
     </div>
