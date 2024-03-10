b4ea8ab5b87cf87568e578a23bb724aba1569b15
G0d-0f-war <tradeveyron@gmail.com>
2024-03-10 22:03:05 +0500

    Update Restaurants.jsx

--- src/widgets/ui/Header/Restaurants/Restaurants.jsx
+++ src/widgets/ui/Header/Restaurants/Restaurants.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RestaurantsEnt from '../../../../entites/ui/SaleWeekend/Restaurants/RestaurantsEnt';
 import styles from './Restaurants.module.scss';
-function Restaurants({ primal,variant}) {
+
+function Restaurants({ primal, variant }) {
   return (
     <div className="container">
-      <RestaurantsEnt primal={primal} variant={variant}/>
+      <RestaurantsEnt primal={primal} variant={variant} />
       <div className={styles.line}></div>
     </div>
   );
