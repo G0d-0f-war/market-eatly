1693d06e791cfa1788cb5f3c2e41fbe12084f56f
G0d-0f-war <tradeveyron@gmail.com>
2024-03-11 22:21:41 +0500

    add slider

--- src/entites/ui/SaleWeekend/Customer/CustomerEnt.jsx
+++ src/entites/ui/SaleWeekend/Customer/CustomerEnt.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import CustomerTitle from './CustomerTitle/CustomerTitle';
 import CustomerReview from './CustomerReview/CustomerReview';
 import styles from './CustomerEnt.module.scss';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 function CustomerEnt(props) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  };
   return (
     <div className={styles.customer}>
-      <CustomerTitle />
-      <CustomerReview />
+      <div className={styles.items}>
+        <CustomerTitle />
+        <Slider {...settings}>
+          <div>
+            <CustomerReview />
+          </div>
+          <div>
+            <CustomerReview />
+          </div>
+          <div>
+            <CustomerReview />
+          </div>
+        </Slider>
+      </div>
     </div>
   );
 }
