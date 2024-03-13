93c0ebe2cf675a6a91561126f151236201abf5fc
souhhh <bulkin-2023@internet.ru>
2024-03-13 22:58:44 +0300

    modalll

--- src/features/ui/Navigation/Modal/Modal.jsx
+++ src/features/ui/Navigation/Modal/Modal.jsx
@@ -1,11 +1,20 @@
-import styles from './Modal.module.scss'
+import styles from './Modal.module.scss';
+import g from '../../../../app/assets/img/modal/G.png';
+import apple from '../../../../app/assets/img/modal/path4.png'
 export default function Modal ({active,setActive}) { 
     return ( 
         <div className={active ? styles.active: styles.modal} onClick={() => setActive(false)}>
             <div className={styles.modal__content} onClick={e => e.stopPropagation()}>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+                <div className={styles.button}>
+                    <button><img src={g} alt="" /></button>
+                    <button><img src={apple} alt="" /></button>
+                </div>
+                <div className={styles.OR}>OR</div>
+                <input type="text" placeholder='FULL NAME' className={styles.input}/>
+                <input type="email" placeholder='EMAIL' className={styles.input}/>
+                <input type="password" placeholder='PASSWORD' className={styles.input}/>
+                <button className={styles.signUp}>SIGN UP</button>
+                <div className={styles.text}>Already Have An Account? <span>Log In</span></div>
             </div>
         </div>
     )
